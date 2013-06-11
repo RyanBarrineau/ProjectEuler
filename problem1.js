2496ef5744d46fea0e0fcf1b67d51c52165208b4
xRYANx <ryan@ryanbarrineau.com>
2013-06-11 13:40:12 -0400

    Create problem1.js

---
+++ problem1.js
@@ -0,0 +1,31 @@
+//Prog: Ryan Barrineau
+//File: Problem1.js
+//Purp: Find the sum of all the multiples of 3 or 5 below 1000
+
+  var sum = 0;
+
+	for(var i = 0; i < 1000; i++)
+  {
+		 if (i % 3 == 0) 
+     {
+	   	  sum += i; 
+		 } 
+     else if(i % 5 == 0) 
+     {
+	   	  sum += i; 
+		 }
+	}
+  
+	document.write(sum);
+	answerCheck(sum);
+  
+function answerCheck(answer) {
+	if(answer == 233168)
+  {
+	  document.write("<br>PASS");
+	} 
+  else 
+  {
+	   document.write("<br>FAIL");
+	}
+}
