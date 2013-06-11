de41b3063d87307afd71ca399de86183928cc1ca
xRYANx <ryan@ryanbarrineau.com>
2013-06-11 13:27:12 -0300

    Create problem3.js

---
+++ problem3.js
@@ -0,0 +1,30 @@
+//Prog:  Ryan Barrineau
+//File:	problem3.html
+//Purp:	Find the largest prime factor of the number 600851475143 ?
+
+	var TESTNUM = 600851475143;
+
+	var newNum = TESTNUM;
+	var factors = new Array();                     
+	var chk = 2;
+	var max = 0;
+                         
+      while (chk*chk <= newNum){ 
+            if (newNum % chk == 0){
+		factors.push(chk);     
+               newNum = newNum/chk;
+            } 
+	    else {
+                  chk++;                     
+            }
+      }
+      if (newNum != 1){                                     
+           factors.push(newNum);          
+      }
+
+	for(var i = 0; factors[i] != null; i++) {
+	   if(factors[i] > max) {
+	     max = factors[i];
+	   }	   
+	}  
+	document.write(max);  
